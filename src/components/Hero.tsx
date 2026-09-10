f4e0fd9817b64fc54e6da3f5e9f472a10dfcc67f
dh1-wsl <237053497+Samz6293@users.noreply.github.com>
2026-09-11 01:00:05 +0600

    Add hero: Add utility classes for text gradient and update Navbar to sticky

---
+++ src/components/Hero.tsx
@@ -0,0 +1,39 @@
+import banner from "../Assets/Banner.png"
+const Hero = () => {
+    return (
+        <>
+            <header className="content-box-s flex flex-col text-center items-center justify-between py-10 gap-4
+            sm:content-box md:flex-row md:text-start md:py-23.5">
+                <div className="flex flex-col gap-4">
+                    <div>
+                        <h1 className="text-3xl font-bold
+                        md:text-5xl md:font-extrabold">Build Your Ideal</h1>
+                        <h1 className="orange-pink-purple text-3xl font-bold
+                        md:text-5xl md:font-extrabold">Development Stack</h1>
+                    </div>
+
+                    <p className="text-[#4B5563] text-[14px] max-w-145
+                    md:text-[18px]">
+                        Explore frontend, backend, database, and tooling
+                        options, compare them side by side, and put together
+                        the stack that fits your next project.
+                    </p>
+
+                    <div className="flex gap-3 font-light justify-center
+                    md:justify-start">
+                        <button className="orange-pink text-white px-4 py-3 rounded-lg">Explore Technologies</button>
+                        <button className="px-8 py-3 outline-2 outline-[#E5E7EB] rounded-lg">Learn More</button>
+                    </div>
+                </div>
+
+                <div>
+                    <img className="md:scale-110" src={banner} alt="Complicated Banner Image" />
+                </div>
+
+
+            </header>
+        </>
+    )
+}
+
+export default Hero
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,17 +3,18 @@ import logo from "../Assets/logo-text.png"
 
 const Nav = () => {
     return (
-        <nav className="border border-gray-200 text-[10px] sm:text-xs">
+        <nav className="sticky top-0 border border-gray-200 text-[10px] 
+        sm:text-xs lg:text-sm">
             <div className="content-box-s grid grid-cols-3 items-center pt-2 pb-1
             sm:p-3 sm:content-box">
-                <CiMenuBurger className="sm:hidden"/>
+                <CiMenuBurger className="md:hidden"/>
 
                 <div className="justify-center">
-                    <img className="block h-5 sm:h-7" src={logo} alt="Dev Stack Logo" />
+                    <img className="block h-5 sm:h-7 lg:h-8" src={logo} alt="Dev Stack Logo" />
                 </div>
 
                 <ul className="hidden text-gray-500 justify-center gap-4
-                sm:flex">
+                md:flex">
                     <li className="hover:text-[#D91B7E]"><a href="#">Home</a></li>
                     <li className="hover:text-[#D91B7E]"><a href="#">Technologies</a></li>
                     <li className="hover:text-[#D91B7E]"><a href="#">Projects</a></li>
@@ -21,9 +22,12 @@ const Nav = () => {
                     <li className="hover:text-[#D91B7E]"><a href="#">Contact</a></li>
                 </ul>
 
-                <div className="flex gap-2 items-center justify-end ">
-                    <button className="font-bold text-gray-600">Sign In</button>
-                    <button className="bg-[#D91B7E] text-white rounded-full px-2 py-0.5 sm:py-1">Sign Up</button>
+                <div className="flex gap-2 items-center justify-end 
+                md:gap-4">
+                    <button className="font-bold text-gray-600 cursor-pointer">Sign In</button>
+                    <button className="bg-[#D91B7E] text-white rounded-full px-3 py-0.5 cursor-pointer
+                    hover:bg-pink-600
+                    sm:py-1">Sign Up</button>
                 </div>
             </div>
         </nav>
